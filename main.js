b784aaccd1236f639186b9932480ba1047b74747
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2022-10-07 10:23:42 -0500

    functions with mugs

---
+++ main.js
@@ -0,0 +1,13 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const bits of clay) {
+   const mug = `${bits} coffee mug`
+   toFireInKiln.push(mug)
+};
+
+console.log(toFireInKiln)
+
+
+
+
